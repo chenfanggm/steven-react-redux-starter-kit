505782882b77daf50d924acc7e01f133e7053275
Chen Fang <chenfanggm@hotmail.com>
2016-10-29 03:38:12 -0700

    [update] travis, dependency

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -121,7 +121,7 @@ webpackConfig.module.loaders = [{
   loader: 'babel',
   query: {
     cacheDirectory: true,
-    plugins: ['transform-runtime', 'lodash', ['antd', { style: 'css' }], 'react-hot-loader/babel'],
+    plugins: ['transform-runtime', 'lodash', ["import", [{ "libraryName": "antd", "style": "css" }]], 'react-hot-loader/babel'],
     presets: ['es2015', 'react', 'stage-0'],
     env: {
       development: {
