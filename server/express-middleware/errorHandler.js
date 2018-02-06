8342e7f0cb1cf81b1a97dbcfef47c0f36b4cb4ab
Chen Fang <chenfanggm@hotmail.com>
2018-02-06 11:53:48 -0800

    [clean] remove import syntax from server

--- server/express-middleware/errorHandler.js
+++ server/express-middleware/errorHandler.js
@@ -1,14 +1,15 @@
-import httpStatus from 'http-status'
-import config from '../../config'
+const httpStatus = require('http-status');
+const config = require('../../config');
+
 
 const errorHandler = () => (err, req, res, next) => {
   if (res.headersSent) {
-    return next(err)
+    return next(err);
   }
   res.status(err.status).json({
     message: err.isPublic ? err.message : httpStatus[err.status],
     stack: config.env === 'development' ? err.stack : {}
-  })
-}
+  });
+};
 
-export default errorHandler
+module.exports = errorHandler;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-import path from 'path'
-import webpack from 'webpack'
-import config from '../config'
-import webpackConfig from '../config/webpack.config'
-import express from 'express'
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import httpStatus from 'http-status'
-import errorHandler from './express-middleware/errorHandler'
-import APIError from './APIError'
-import _debug from 'debug'
+const path = require('path');
+const webpack = require('webpack');
+const config = require('../config');
+const webpackConfig = require('../config/webpack.config');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const httpStatus = require('http-status');
+const errorHandler = require('./express-middleware/errorHandler');
+const APIError = require('./APIError');
+const _debug = require('debug');
+const debug = _debug('app:server');
 
-const debug = _debug('app:server')
 
-debug('Init express app...')
-const app = express()
+debug('Init express app...');
+const app = express();
 // ------------------------------------
 // Apply Webpack HMR Middleware
 // ------------------------------------
 
 if (config.env === 'development') {
-  const compiler = webpack(webpackConfig)
+  const compiler = webpack(webpackConfig);
 
-  debug('Enabling webpack development and HMR middleware')
+  debug('Enabling webpack development and HMR middleware');
   app.use(require('webpack-dev-middleware')(compiler, {
     publicPath  : webpackConfig.output.publicPath,
     contentBase : path.resolve(config.baseDir, config.clientDir),
@@ -30,32 +30,32 @@ if (config.env === 'development') {
     noInfo      : false,
     lazy        : false,
     stats       : 'normal',
-  }))
+  }));
   app.use(require('webpack-hot-middleware')(compiler, {
     path: '/__webpack_hmr'
-  }))
+  }));
 
-  app.use(express.static(path.resolve(config.baseDir, config.staticDir)))
+  app.use(express.static(path.resolve(config.baseDir, config.staticDir)));
 
   // fallback all routes to index.html
   app.use('*', function (req, res, next) {
-    const filename = path.join(compiler.outputPath, 'index.html')
+    const filename = path.join(compiler.outputPath, 'index.html');
     compiler.outputFileSystem.readFile(filename, (err, result) => {
       if (err) {
-        return next(err)
+        return next(err);
       }
-      res.set('content-type', 'text/html')
-      res.send(result)
-      res.end()
-    })
-  })
+      res.set('content-type', 'text/html');
+      res.send(result);
+      res.end();
+    });
+  });
 
-  debug('Server is running on DEVELOPMENT mode.')
+  debug('Server is running on DEVELOPMENT mode.');
 } else {
   app.use(express.static(path.resolve(config.baseDir, config.distDir), {
     maxage: config.cache_control.max_age
-  }))
-  debug('Server is running on PRODUCTION mode.')
+  }));
+  debug('Server is running on PRODUCTION mode.');
 }
 
 app.use(cookieParser());
@@ -65,16 +65,16 @@ app.use(bodyParser.json());
 // error transform
 app.use(function (err, req, res, next) {
   if (Array.isArray(err)) {
-    const unifiedErrorMessage = err.map(function (error) { return error.msg }).join(' and ')
-    const error = new APIError(unifiedErrorMessage, httpStatus.BAD_REQUEST, true)
-    return next(error)
+    const unifiedErrorMessage = err.map(function (error) { return error.msg; }).join(' and ');
+    const error = new APIError(unifiedErrorMessage, httpStatus.BAD_REQUEST, true);
+    return next(error);
   } else if (!(err instanceof APIError)) {
-    return next(new APIError(err.message, err.status, err.isPublic))
+    return next(new APIError(err.message, err.status, err.isPublic));
   }
-  return next(err)
-})
+  return next(err);
+});
 
 // error handler
-app.use(errorHandler())
+app.use(errorHandler());
 
-module.exports = app
+module.exports = app;
